eee7b3ed3ed83c2c143d2d1e530a89a64151abe0
camlischke1 <camlischke@gmail.com>
2021-04-14 09:52:10 -0400

    lengthened training for rr

--- RoadRunner/stats.py
+++ RoadRunner/stats.py
@@ -5,11 +5,11 @@ from tensorflow.keras.models import load_model
 
 
 colored = np.load("history_RR_State_Conv2D.npy", allow_pickle=True).item()
-bw = np.load("history_RR_State_Conv2D.npy", allow_pickle=True).item()
+bw = np.load("history_RR_State_Conv2D150.npy", allow_pickle=True).item()
 
 
-plt.plot(colored['val_mae'], label='30')
-plt.plot(bw['val_mae'], label='60')
+plt.plot(colored['val_mae'], label='60')
+plt.plot(bw['val_mae'], label='150')
 plt.title("RR State Validation MAE")
 plt.legend()
 plt.show()
